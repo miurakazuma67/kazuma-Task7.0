e2911022723f954e07d4a06124045da916cef279
miurakazuma67 <68637266+miurakazuma67@users.noreply.github.com>
2021-04-24 16:45:28 +0900

    1回目提出

---
+++ Kazuma-Task7/MinusViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MinusViewController.swift
+//  Kazuma-Task7
+//
+//  Created by 三浦　一真 on 2021/04/24.
+//
+
+import UIKit
+
+class MinusViewController: UIViewController {
+
+    @IBOutlet private weak var textField1: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
+    @IBOutlet private weak var resultLabel: UILabel!
+    
+    @IBAction func minus(_ sender: Any) {
+        let num1 = Double(textField1.text ?? "")  ?? 0
+        let num2 = Double(textField2.text ?? "")  ?? 0
+        let result = num1 - num2
+        resultLabel.text = String(result)
+    }
+}
