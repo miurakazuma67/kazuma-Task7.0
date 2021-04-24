13437cea8ced81e440525477191aa8846c795dde
miurakazuma67 <68637266+miurakazuma67@users.noreply.github.com>
2021-04-24 14:31:45 +0900

    Initial Commit

---
+++ Kazuma-Task7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kazuma-Task7
+//
+//  Created by 三浦　一真 on 2021/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
